5c31b54d3fdef42eb722f8a131308e6025aa187e
Seinosuke01 <seinosuke.saito520@gmail.com>
2025-03-05 17:58:17 +0900

    Fix: メタデータ取得リレーの初期設定はrelay.damus.ioに限定する

--- DevCamp/Views/Window/SigninView.swift
+++ DevCamp/Views/Window/SigninView.swift
@@ -77,11 +77,12 @@ struct SigninView: View {
     }
     
     private func addRelay() async {
+//      Setting up more relays will result in laggy
         let metadataRelayUrls = [
             "wss://relay.damus.io",
-            "wss://nostr.land",
-            "wss://yabu.me",
-            "wss://nos.lol",
+//            "wss://nostr.land",
+//            "wss://yabu.me",
+//            "wss://nos.lol",
         ]
         let nip29relayUrl = "wss://groups.0xchat.com"
         
